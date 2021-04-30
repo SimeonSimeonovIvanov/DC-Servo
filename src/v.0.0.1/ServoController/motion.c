7328754086bbbdbf9d39fdb155b48d4de8ab767f
SimeonSimeonovIvanov <simeon_s._ivanov@abv.bg>
2021-04-30 06:47:18 +0300

    Add Copyright (Frank Tkalcevic, www.franksworkshop.com) for motion.c and motion.h

--- src/v.0.0.1/ServoController/motion.c
+++ src/v.0.0.1/ServoController/motion.c
@@ -1,3 +1,19 @@
+// MotorDriver2, firmware for the I2C Servo Motor Controller
+// Copyright (C) 2008, Frank Tkalcevic, www.franksworkshop.com
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "motion.h"
 
 static enum EState nRunState;
